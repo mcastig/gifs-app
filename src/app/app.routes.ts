e837a5a6fddc4b512e76c696570d432becdc64f0
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-02 21:53:59 -0600

    Add complete functionality for dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,21 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'dashboard',
-    loadComponent: () => import('./gifts/pages/dashboard-page/dashboard-page.component')
+    loadComponent: () => import('./gifts/pages/dashboard-page/dashboard-page.component'),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifts/pages/trending-page/trending-page.component')
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifts/pages/search-page/search-page.component')
+      },
+      {
+        path: '**',
+        redirectTo: 'trending'
+      }
+    ]
   },
   {
     path: '**',
---
+++ src/app/gifts/components/side-menu/side-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SideMenuOptionsComponent } from "./side-menu-options/side-menu-options.component";
+import { SideMenuHeaderComponent } from "./side-menu-header/side-menu-header.component";
+
+@Component({
+  selector: 'gifts-side-menu',
+  imports: [SideMenuOptionsComponent, SideMenuHeaderComponent],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.css'
+})
+export class SideMenuComponent { }
