c2d687e5d1e62eadb3f1d3ae3950b40952b104b7
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-03 01:01:56 -0600

    Create a mapper to format the respond of service

--- src/app/gifts/services/gifs.service.ts
+++ src/app/gifts/services/gifs.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
+import type { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
   private http = inject(HttpClient);
+  public trendingGifs = signal<Gif[]>([]);
 
   constructor() {
     this.loadTrendingGifs();
@@ -19,6 +22,10 @@ export class GifsService {
         api_key: environment.giphyApiKey,
         limit: 20
       },
-    });
+    }).subscribe((resp) => {
+       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+       this.trendingGifs.set(gifs);
+       console.log({gifs});
+    })
   }
 }
